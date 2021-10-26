6a53ae0767223fe8c9ceaa58d8e2cd40ce1a353b
Bee7go <bianca_badaruta@yahoo.com>
2021-10-26 16:30:55 +0300

    lab 1,2,3 - completed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 class Pair:
     def __init__(self, key, value):
         self.key = key
@@ -24,15 +26,14 @@ class AlphabeticallySortedST:
     # returns the position of the key if it already exists in our sorted symbol table and -1 otherwise
     def checkIfElementExists(self, pair):
         for i in range(len(self.list)):
-            # if self.list[i].getKey() <= pair.getKey():
+            if self.list[i].getKey() > pair.getKey():
+                return -1
             if self.list[i].getKey() == pair.getKey():
                 return self.list[i].getValue()
-            # else:
-            #    return -1
         return -1
 
     # adds new element in the table (if the key doesn't exists already)  so that the list keeps it's sorted property
-    # returns -1 if element doesn't exist and it's position in list otherwise
+    # returns -1 if element doesn't exist and it's dictionary value otherwise
     def addElement(self, token):
         pair = Pair(token, len(self.list)+1)
         for i in range(len(self.list)):
@@ -41,7 +42,6 @@ class AlphabeticallySortedST:
             if self.list[i].getKey() > pair.getKey():
                 self.list.insert(i, pair)
                 return
-        # in case our list is empty:
         self.list.append(pair)
 
     # returns the alphabetically sorted symbol table
@@ -62,9 +62,9 @@ class Scanner:
     def __iter__(self):
         return self
 
-    def genPIF(self, token, index):
+    def genPIF(self, classification, token, index):
         pair = Pair(token, index)
-        self.pif.append(pair)
+        self.pif.append([classification, pair])
 
     def getPIF(self):
         return self.pif
@@ -76,28 +76,34 @@ class Scanner:
                 return 0
             self.line = self.line.split()
             self.lineCount += 1
+            self.tokenCount = -1
         self.tokenCount += 1
         currentToken = self.line.pop(0)
-        # self.prevToken = self.classifyToken(currentToken)
         return currentToken
 
     def codifyToken(self, currentToken):
         pair = Pair(currentToken, 0)
         if self.symbolTable.checkIfElementExists(pair) != -1:
             return 1
+
         with open("files/token.in") as f:
             for line in f:
                 splitedLine = line.split()
                 if currentToken == splitedLine[0]:
                     return int(splitedLine[1])
 
-        if 3 <= self.prevToken <= 8:
-            self.symbolTable.addElement(currentToken)
-            return 1
+        #check if identifier
+        if re.match("^([a-zA-Z_$][a-zA-Z\\d_$]*)$", currentToken):
+            if 3 <= self.prevToken <= 8 or self.prevToken == 10:
+                self.symbolTable.addElement(currentToken)
+                return 1
 
-        if currentToken == 'yes' or 'no' or currentToken.isnumeric() or (
-                currentToken.startsWith("[[") and currentToken.endswith("]]")) \
-                or (currentToken.startsWith("'") and currentToken.endsWith("'")):
+        #check if constant
+        if currentToken == 'yes' or currentToken == 'no' or currentToken.isdigit() or \
+                (currentToken.startswith('-') and currentToken[1:].isdigit()) or \
+            (currentToken.startswith("[[") and currentToken.endswith("]]")) \
+                or (currentToken.startswith("'") and currentToken.endswith("'")) \
+            or (currentToken.startswith("\"") and currentToken.endswith("\"")):
             self.symbolTable.addElement(currentToken)
             return 2
         return -1
@@ -107,47 +113,42 @@ class Scanner:
             return 'identifier'
         if code == 2:
             return 'constant'
-        if 3 <= code <= 14:
+        if 3 <= code <= 15:
             return 'reserved-word'
-        if 15 <= code <= 23:
+        if 16 <= code <= 24:
             return 'operator'
-        if 24 <= code <= 27:
+        if 25 <= code <= 26:
             return 'separator'
         return 'lexical error'
 
 
 if __name__ == '__main__':
-    myScanner = Scanner(open("files/p2.in"))
-
+    myScanner = Scanner(open("files/p1.in"))
     currentToken = myScanner.detectNextToken()
-    line = 0
+    error = 0
     while currentToken:
-        #print(currentToken)
         tokenClassified = myScanner.classifyCodification(myScanner.codifyToken(currentToken))
-        #print(tokenClassified + ' line:' + str(myScanner.lineCount) + ' token:' + str(myScanner.tokenCount))
+        if tokenClassified == 'lexical error':
+            print("LEXICAL ERROR AT LINE " + str(myScanner.lineCount) + ' TOKEN ' + str(myScanner.tokenCount))
+            error = 1
         if tokenClassified == 'reserved-word' or tokenClassified == 'operator' or tokenClassified == 'separator':
-            myScanner.genPIF(currentToken, 0)
+            myScanner.genPIF(tokenClassified,currentToken, 0)
         else:
             if tokenClassified == 'identifier' or tokenClassified == 'constant':
                 pair = Pair(currentToken, 0)
-                myScanner.genPIF(currentToken, myScanner.symbolTable.checkIfElementExists(pair))
+                myScanner.genPIF(tokenClassified, currentToken, myScanner.symbolTable.checkIfElementExists(pair))
         myScanner.prevToken = myScanner.codifyToken(currentToken)
         currentToken = myScanner.detectNextToken()
 
-    print("- - - - SYMBOL TABLE - - - - - - -")
     f = open("files/ST.out", "w")
     for element in myScanner.symbolTable.getList():
-        print(element.getKey() + ' ' + str(element.getValue()))
         f.write(element.getKey() + ' ' + str(element.getValue()) + '\n')
     f.close()
 
-    print("- - - - PIF - - - - - - -")
     f = open("files/PIF.out", "w")
     for el in myScanner.pif:
-        print(el.getKey() + ' ' + str(el.getValue()))
-        f.write(el.getKey() + ' ' + str(el.getValue()) + '\n')
+        f.write(el[0] + ' ' + el[1].getKey() + ' ' + str(el[1].getValue()) + '\n')
     f.close()
 
-
-    # print(myScanner.symbolTable.list)
-    # print(myScanner.detectNextToken())
+    if not error:
+        print("LEXICALLY CORRECT")
